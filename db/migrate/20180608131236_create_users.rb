682251422b0e9c3e74c0884d7e128a4c6d0e4bf3
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-08 22:15:07 +0900

    Add user table

---
+++ db/migrate/20180608131236_create_users.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name,  null: false
+      t.string :email
+      t.text   :image
+
+      t.timestamps
+    end
+  end
+end
