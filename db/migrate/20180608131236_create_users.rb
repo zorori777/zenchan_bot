6f063c62918ca4b6a2c82330072791ccf4c83010
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-10 11:17:01 +0900

    Remove email column for unnessary

--- db/migrate/20180608131236_create_users.rb
+++ db/migrate/20180608131236_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name,  null: false
-      t.string :email
       t.text   :image
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2018_06_08_131236) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
