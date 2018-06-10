8aa902b25d90707f49c92ca83e169a1d5f2ad3ba
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-10 11:18:02 +0900

    Remove email column for unnessary in factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "MR.Test#{n}"}
-    sequence(:email) { |n| "MR.Test#{n}@example.com"}
     image "https://cdn.pixabay.com/photo/2015/06/15/01/04/water-809799_960_720.jpg"
   end
 end
