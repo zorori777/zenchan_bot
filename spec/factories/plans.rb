d94187576ebba4638e09e1c9c5b4d972757cae5f
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-10 15:06:37 +0900

    Add test data by factory

---
+++ spec/factories/plans.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :plan do
+    sequence(:title) { |n| "あなたとデート ~part#{n}~"}
+    appointed_place "新宿駅"
+    executed_status "0"
+    appointed_at { Time.zone.tomorrow }
+    executed_at { Time.zone.tomorrow }
+    user
+  end
+end
