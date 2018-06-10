655b394da02bbed95aa40b19a4660be8813eeb49
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-10 19:53:05 +0900

    Add todo table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_032938) do
+ActiveRecord::Schema.define(version: 2018_06_10_103048) do
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 2018_06_10_032938) do
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
+  create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "image"
+    t.text "url"
+    t.string "caution"
+    t.integer "eager_status", limit: 1, default: 0, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title", "eager_status"], name: "index_todos_on_title_and_eager_status"
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "image"
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_06_10_032938) do
   end
 
   add_foreign_key "plans", "users"
+  add_foreign_key "todos", "users"
 end
