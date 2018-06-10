3b714ade3079f3c4ddcfc649f2b1ba988dd30255
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-10 15:59:09 +0900

    Fix test data to resolve error

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :plan do
     sequence(:title) { |n| "あなたとデート ~part#{n}~"}
     appointed_place "新宿駅"
-    executed_status "0"
+    executed_status 0
     appointed_at { Time.zone.tomorrow }
     executed_at { Time.zone.tomorrow }
     user
