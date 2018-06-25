7cd50fe0d021ebe28e7993bab36bc9b922f2fa9b
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-25 21:47:00 +0900

    Add create action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,5 +10,24 @@ class TodosController < ApplicationController
   end
 
   def create
+    @todo = current_user.todos.new(create_params)
+    if @todo.save
+      redirect_to todos_path, notice: "作成されました"
+    else
+      flash.now[:alert] = "作成を失敗しました "
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:todo).permit(
+      :title,
+      :url,
+      :caution,
+      :image,
+      :eager_status
+    )
   end
 end
