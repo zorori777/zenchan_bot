9cba000b67e7c345655bb658e5ed6f9536ce9751
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-25 21:39:42 +0900

    Add association with activestorage and validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
+  enum eager_status: { "とりあえず": 0, "興味あり": 1, "絶対行く": 2 }
+
   belongs_to :user
   has_many   :todo_plans, dependent: :destroy
   has_many   :plans, through: :todo_plans
   has_many   :todo_comments, dependent: :destroy
   has_many   :likes, as: :likeable, dependent: :destroy
+  has_one_attached :image
 
-  enum eager_status: { "とりあえず": 0, "興味あり": 1, "絶対行く": 2 }
+  validates :title, presence: true
 end
