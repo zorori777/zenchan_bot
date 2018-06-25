cb8c81b6e65b84065386ac341b217fc76f5baa8c
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-25 20:38:26 +0900

    Add enum to todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -6,4 +6,6 @@ class Todo < ApplicationRecord
   has_many   :plans, through: :todo_plans
   has_many   :todo_comments, dependent: :destroy
   has_many   :likes, as: :likeable, dependent: :destroy
+
+  enum eager_status: { "とりあえず": 0, "興味あり": 1, "絶対行く": 2 }
 end
