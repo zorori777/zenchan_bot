f7fb134d22826da7de0ff9ca1db8eb79d2316683
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-06-27 15:00:42 +0900

    Add config to use local font in ja

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module ZenchanBot
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
